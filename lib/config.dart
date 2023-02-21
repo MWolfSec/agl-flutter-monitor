0f912625f4f2484a27f8b65b99ff74d392e3763f
MWolfAVL <125113761+MWolfSec@users.noreply.github.com>
2023-02-21 13:30:34 +0100

    fixed name error in config
    fixed name error in config

--- lib/config.dart
+++ lib/config.dart
@@ -22,7 +22,7 @@ class _GetConfigState extends ConsumerState<GetConfig> {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       final configStateProvider = ref.read(ConfigStateprovider.notifier);
 
-      String configFilePath = '/etc/xdg/AGL/monitor_config.yaml';
+      String configFilePath = '/etc/xdg/AGL/MONITOR_config.yaml';
 
 
       final configFile = File(configFilePath);
