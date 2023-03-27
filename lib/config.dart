dc7e59ea0a53b44ed638f5dde9c6aae8c05f085c
MWolfSec <125113761+MWolfSec@users.noreply.github.com>
2023-03-27 11:03:05 +0200

    edited config name
    edited config name

--- lib/config.dart
+++ lib/config.dart
@@ -21,7 +21,7 @@ class _GetConfigState extends ConsumerState<GetConfig> {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       final configStateProvider = ref.read(ConfigStateprovider.notifier);
       //for deployment on AGL use /etc/xdg path
-      String configFilePath = '/etc/xdg/AGL/Monitor_config.yaml';
+      String configFilePath = '/etc/xdg/AGL/MONITOR_config.yaml';
       //String configFilePath = 'extras/Monitor_config.yaml';
 
       final configFile = File(configFilePath);
